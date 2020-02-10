42a0880d46e79ac7d04a0e242393ee8a8a73d1d5
Seth Shelnutt <Shelnutt2@gmail.com>
2020-02-09 20:07:25 -0500

    Initial commit with base implementation

---
+++ setup.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+"""
+    TileDB JupyterLab Contents plugin
+"""
+
+from setuptools import setup, find_namespace_packages
+
+NAME = "tiledb-contents"
+
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+    "notebook",
+    "ipykernel",
+    "setuptools>=18.0",
+    "setuptools-scm>=1.5.4",
+    "setuptools-scm-git-archive",
+    "tiledb>=0.5.6",
+    "tiledb-cloud>=0.5.5",
+]
+
+setup(
+    name=NAME,
+    description="TileDB Contents Plugin for storing Jupyterlab Notebooks in TileDB Arrays",
+    author_email="hello@tiledb.com",
+    url="https://tiledb.com",
+    keywords=["TileDB", "cloud", "jupyter", "notebook"],
+    install_requires=REQUIRES,
+    packages=find_namespace_packages(include=["tiledbcontents"]),
+    include_package_data=True,
+    zip_safe=True,
+    use_scm_version={
+        "version_scheme": "guess-next-dev",
+        "local_scheme": "dirty-tag",
+        "write_to": "tiledbcontents/version.py",
+    },
+    long_description="""\
+    TileDB Contents Plugin for storing Jupyterlab Notebooks in TileDB Arrays
+    """,
+)
