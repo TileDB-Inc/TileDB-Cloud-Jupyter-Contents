6b87cf8fdc9f7e054b1f6a200cc84e4cf723bde5
Paul Fisher <paul.fisher@tiledb.com>
2022-03-04 18:29:16 -0500

    Don't require storage credentials when creating an array.

--- tiledbcontents/tiledbcontents.py
+++ tiledbcontents/tiledbcontents.py
@@ -550,7 +550,12 @@ class TileDBCloudContentsManager(TileDBContents, filemanager.FileContentsManager
                 options_json = json.loads(options)
                 model["name"] = options_json["name"]
                 model["tiledb:s3_prefix"] = options_json["s3_prefix"]
-                model["tiledb:s3_credentials"] = options_json["s3_credentials"]
+                try:
+                    model["tiledb:s3_credentials"] = options_json["s3_credentials"]
+                except KeyError:
+                    # Enterprise users might use a backend where credentials
+                    # are not required.
+                    pass
             except Exception as e:
                 raise tornado.web.HTTPError(
                     400, u"Could not read TileDB user options: {}".format(e)
